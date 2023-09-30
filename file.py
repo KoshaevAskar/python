5af6ce243b9dff90684e08239a51fedd42b8e776
Karim <karimmak@gmail.com>
2023-09-30 17:47:37 +0300

    first python project

---
+++ file.py
@@ -0,0 +1,15 @@
+print (5466665)
+print(5+5)
+print(type(5))
+print('ПАПА КРИКНУЛ:"ЭХ"')
+print("hello")
+print("Hello "+"World")#конкатенация
+# login = input("Введите логин: ")
+# pessword = input("Введите пароль:  ")
+# me = "Askar"
+# print  (login, pessword, me)i
+side = input("Введите сторону квадрата")
+side = int (side)
+s = side * side
+print("Площфдь квадрата равна"  + str (s))
+
